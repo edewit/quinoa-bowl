8f0c6bd4dbd3fa50418460ded2f94dd0336cb4a3
Selamet Aydogdu <sa@pcport.ch>
2022-06-16 16:01:46 +0200

    add latitude and longitude

--- src/main/java/hackathon/lunchgate/RestaurantResource.java
+++ src/main/java/hackathon/lunchgate/RestaurantResource.java
@@ -4,15 +4,12 @@ package hackathon.lunchgate;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.RestQuery;
 
 import io.smallrye.common.annotation.Blocking;
 
 @Path("/restaurant")
-@RegisterRestClient
-@ClientHeaderParam(name = "Authorization", value = "{lookupAuth}")
 public class RestaurantResource {
 
 	 @RestClient
@@ -21,7 +18,9 @@ public class RestaurantResource {
 	    @GET
 	    @Path("list")
 	    @Blocking
-	    public Restaurants list() {
-	        return lunchGate.list("zurich", "CH", "de", "json");
+	    public Restaurants list(@RestQuery String latitude, @RestQuery String longitude) {
+	        return lunchGate.list(null, null, "de", "json", latitude, longitude);
 	    }
+	    
+	    
 }
